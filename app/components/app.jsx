655d66418e037355285df79b09d733be05a545a3
Charles F. Munat <charles.munat@gmail.com>
2016-01-12 12:21:04 +1300

    Add svg

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 class App extends Component {
 
   render () {
-    return <div>App!</div>
+    return <svg>
+      <circle cx={50} cy={50} r={10} fill="red" />
+    </svg>
   }
 }
 
