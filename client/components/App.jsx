cda62c51c8e2a2c49dd37987fddc8fce2f1f4a4a
Don Smith <locksmithdon@gmail.com>
2017-04-20 13:25:15 +1200

    render each circle separately

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,50 +2,20 @@ import React from 'react'
 
 import Circle from './Circle'
 
-class App extends React.Component {
+const circle = {
+  cx: 400,
+  cy: 400,
+  r: 256,
+  level: 0,
+  isOn: false
+}
 
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      circles: [{ cx: 400, cy: 400, r: 256, level: 0, isOn: false }]
-    }
-
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick (circleClicked) {
-    if (circleClicked.isOn) return
-
-    const { cx, cy, r } = circleClicked
-    const level = circleClicked.level + 1
-
-    const newCircle = Object.assign({}, circleClicked)
-    newCircle.isOn = true
-
-    const circles = this.state.circles.map(circle => {
-      return circleClicked.cx === circle.cx && circleClicked.cy === circle.cy ? newCircle : circle
-    })
-
-    circles.push(
-      { cx: cx, cy: cy - r, r: r / 2, level, isOn: false }, // north
-      { cx: cx, cy: cy + r, r: r / 2, level, isOn: false }, // south
-      { cx: cx + r, cy: cy, r: r / 2, level, isOn: false }, // east
-      { cx: cx - r, cy: cy, r: r / 2, level, isOn: false }  // west
-    )
-
-    this.setState({ circles })
-  }
-
-  render () {
-    return (
-      <svg width='800' height='800'>
-        {this.state.circles.map((circle, i) => {
-          return <Circle key={i} circle={circle} onClick={this.handleClick} />
-        })}
-      </svg>
-    )
-  }
+const App = () => {
+  return (
+    <svg width='800' height='800'>
+      <Circle circle={circle} />
+    </svg>
+  )
 }
 
 export default App
--- client/components/Circle.jsx
+++ client/components/Circle.jsx
@@ -2,13 +2,51 @@ import React from 'react'
 
 import { getColor } from '../color'
 
-const Circle = props => {
-  const { cx, cy, r, level } = props.circle
-  const color = getColor(level)
+class Circle extends React.Component {
 
-  return (
-    <circle cx={cx} cy={cy} r={r} fill={color} onMouseOver={() => props.onClick(props.circle)} />
-  )
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      circle: props.circle,
+      children: []
+    }
+
+    this.handleClick = this.handleClick.bind(this, props.circle)
+  }
+
+  handleClick (eventedCircle) {
+    if (eventedCircle.isOn) return
+
+    const children = []
+    const { cx, cy, r } = eventedCircle
+    const level = eventedCircle.level + 1
+    const circle = Object.assign({}, eventedCircle)
+    circle.isOn = true
+
+    children.push(
+      { cx: cx, cy: cy - r, r: r / 2, level, isOn: false }, // north
+      { cx: cx, cy: cy + r, r: r / 2, level, isOn: false }, // south
+      { cx: cx + r, cy: cy, r: r / 2, level, isOn: false }, // east
+      { cx: cx - r, cy: cy, r: r / 2, level, isOn: false }  // west
+    )
+
+    this.setState({ circle, children })
+  }
+
+  render () {
+    const { cx, cy, r, level } = this.state.circle
+    const color = getColor(level)
+
+    return (
+      <g>
+        <circle cx={cx} cy={cy} r={r} fill={color} onMouseOver={this.handleClick} />
+        {this.state.children.map((circle, i) => {
+          return <Circle key={i} circle={circle} />
+        })}
+      </g>
+    )
+  }
 }
 
 export default Circle
